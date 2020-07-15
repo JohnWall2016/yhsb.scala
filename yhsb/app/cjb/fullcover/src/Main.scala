6c284ca7dd0d578f9f8801d190644dc42f5e3c37
JohnWall2016 <5672618@qq.com>
2020-07-15 15:20:12 +0800

    Update

--- yhsb/app/cjb/fullcover/src/Main.scala
+++ yhsb/app/cjb/fullcover/src/Main.scala
@@ -3,11 +3,15 @@ package yhsb.app.cjb.fullcover
 import org.rogach.scallop._
 
 class Conf(args: Seq[String]) extends ScallopConf(args) {
-  val updateDwxx = new Subcommand("upDwxx") {
-    descr("更新单位信息")
-
+  trait RowIndex { _: ScallopConf =>
     val beginRow = trailArg[Int](descr = "开始行")
     val endRow = trailArg[Int](descr = "结束行")
+  }
+
+  val updateDwxx = new Subcommand("upDwxx") with RowIndex {
+    descr("更新单位信息")
+
+    val row = trailArg[String](descr = "列名称")
   }
   addSubcommand(updateDwxx)
   verify()
@@ -20,7 +24,7 @@ object Main {
     conf.subcommand match {
       case Some(conf.updateDwxx) => {
         import conf.updateDwxx._
-        println(s"begin: $beginRow, end: $endRow")
+        println(s"begin: $beginRow, end: $endRow, row: $row")
       }
       case _ => {
         conf.printHelp()
