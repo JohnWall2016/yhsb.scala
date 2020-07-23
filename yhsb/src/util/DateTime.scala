74d11eb212f2be6b6db2adf7cde0313908a488b9
JohnWall2016 <5672618@qq.com>
2020-07-23 17:50:52 +0800

    Update

--- yhsb/src/util/DateTime.scala
+++ yhsb/src/util/DateTime.scala
@@ -4,6 +4,7 @@ import scala.util.matching.Regex
 import java.{util => ju}
 import java.text.SimpleDateFormat
 import java.util.Date
+import scala.collection.mutable
 
 object DateTime {
   def toDashedDate(
@@ -20,4 +21,24 @@ object DateTime {
       date: Date = new Date(),
       format: String = "yyyyMMdd"
   ): String = new SimpleDateFormat(format).format(date)
+}
+
+case class Month(year: Int, month: Int) extends Ordering[Month] {
+  override def compare(x: Month, y: Month): Int = {
+    val yearDelta = x.year - y.year
+    if (yearDelta < 0) -1
+    else if (yearDelta > 0) 1
+    else {
+      val monthDelta = x.month - y.month
+      if (monthDelta < 0) -1
+      else if (monthDelta > 0) 1
+      else 0
+    }
+  }
+}
+
+case class MonthRange(start: Month, end: Month) {
+  def -(that: MonthRange): Option[Seq[Month]] = {
+    ???
+  }
 }
