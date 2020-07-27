8c582c4d9302c418877369b66fce67642b19b348
JohnWall2016 <5672618@qq.com>
2020-07-27 17:57:23 +0800

    Update

--- yhsb/app/cjb/fullcover/src/Main.scala
+++ yhsb/app/cjb/fullcover/src/Main.scala
@@ -6,17 +6,25 @@ import yhsb.util.commands.InputFile
 import yhsb.util.Excel
 import yhsb.util.Excel._
 import yhsb.cjb.db.FullCover._
+import yhsb.util.commands.OutputDir
 
 class Conf(args: Seq[String]) extends ScallopConf(args) {
   banner("全覆盖数据处理程序")
 
-  val updateDwxx = new Subcommand("upDwxx") with InputFile with RowRange {
-    descr("更新单位信息")
+  val updateDwmc = new Subcommand("upDwmc") with InputFile with RowRange {
+    descr("更新单位名称")
 
     val idcardRow = trailArg[String](descr = "身份证列名称")
     val dwxxRow = trailArg[String](descr = "单位信息列名称")
   }
-  addSubcommand(updateDwxx)
+
+  val downloadByDwmc = new Subcommand("downByDw") with OutputDir {
+    descr("按单位导出下发数据")
+  }
+
+  addSubcommand(updateDwmc)
+  addSubcommand(downloadByDwmc)
+
   verify()
 }
 
@@ -25,8 +33,8 @@ object Main {
     val conf = new Conf(args)
 
     conf.subcommand match {
-      case Some(conf.updateDwxx) => {
-        import conf.updateDwxx._
+      case Some(conf.updateDwmc) => {
+        import conf.updateDwmc._
         println(
           s"begin: ${startRow()}, end: ${endRow()}, " +
           s"idcardRow: ${idcardRow()}, dwxxRow: ${dwxxRow()}"
@@ -51,6 +59,20 @@ object Main {
           println(s"${index+1} $idcard $dwxx")
           update(idcard, dwxx)
         }
+      }
+      case Some(conf.downloadByDwmc) => {
+        import conf.downloadByDwmc._
+
+        import fullcover._
+
+        val result = run(
+          fc2Stxfsj.groupBy(_.dwmc).map(e => (e._1, e._2.size))
+        )
+
+        for ((name, count) <- result) {
+          println(s"$name $count")
+        }
+
       }
       case _ => {
         conf.printHelp()
--- yhsb/src/util/Strings.scala
+++ yhsb/src/util/Strings.scala
@@ -9,4 +9,49 @@ object Strings {
       fileName + appendString
     }
   }
+
+  case class CharRange(start: Char, end: Char) {
+    if (start > end)
+      throw new IllegalArgumentException("start must be <= end")
+
+    def contains(ch: Char) = ch >= start && ch <= end
+  }
+
+  case class SpecialChars(range: CharRange, width: Int)
+
+  private val chineseChars = SpecialChars(
+    CharRange('\u4e00', '\u9fa5'),
+    2
+  )
+
+  private def padCount(
+      s: String,
+      width: Int,
+      specialChars: Seq[SpecialChars]
+  ): Int = {
+    s.foldLeft(0) { (count, char) =>
+      count + specialChars
+        .find(sp => sp.range.contains(char))
+        .map(_.width)
+        .getOrElse(1)
+    }
+  }
+
+  implicit class StringOps(val s: String) extends AnyVal {
+    def padRight(
+        width: Int,
+        padChar: Char = ' ',
+        specialChars: Seq[SpecialChars] = Seq(chineseChars)
+    ): String = {
+      val count = padCount(s, width, specialChars)
+      if (count > 0) {
+        val b = new StringBuilder
+        b.append(s)
+        b.append(padChar * count)
+        b.toString()
+      } else {
+        s
+      }
+    }
+  }
 }
