8b9069526b848074d48dde2ccff3a9b957d64bff
JohnWall2016 <5672618@qq.com>
2020-11-09 12:54:09 +0800

    Update

--- yhsb/app/cjb/query/src/Main.scala
+++ yhsb/app/cjb/query/src/Main.scala
@@ -9,10 +9,10 @@ import yhsb.cjb.net.protocol.Cbxx
 import yhsb.util.Files.appendToFileName
 
 class Query(args: Seq[String]) extends Command(args) {
-  
+
   banner("数据查询处理程序")
 
-  addSubCommand {
+  val doc =
     new Subcommand("doc") with InputFile {
       descr("档案目录生成")
 
@@ -34,17 +34,57 @@ class Query(args: Seq[String]) extends Command(args) {
             } else {
               val cbxx = result(0)
               println(s"${i + 1} ${cbxx.name}")
-              
-              row.getOrCreateCell("E").setCellValue(
-                s"${cbxx.name}$title"
-              )
+
+              row
+                .getOrCreateCell("E")
+                .setCellValue(
+                  s"${cbxx.name}$title"
+                )
             }
           }
         }
         workbook.save(appendToFileName(inputFile(), ".upd"))
       }
     }
-  }
+
+  val up =
+    new Subcommand("up") with InputFile with RowRange {
+      descr("更新参保信息")
+
+      val idcardRow = trailArg[String](descr = "身份证列名称")
+      val updateRow = trailArg[String](descr = "更新列名称")
+
+      def execute(): Unit = {
+        val workbook = Excel.load(inputFile())
+        val sheet = workbook.getSheetAt(0)
+
+        Session.use() { session =>
+          for (i <- (startRow() - 1) until endRow()) {
+            val row = sheet.getRow(i)
+            val idcard = row.getCell(idcardRow()).value.trim().toUpperCase()
+
+            println(idcard)
+
+            session.sendService(CbxxRequest(idcard))
+            val result = session.getResult[Cbxx]()
+
+            row
+              .getOrCreateCell(updateRow())
+              .setCellValue {
+                if (result.isEmpty || result(0).idcard == null) {
+                  ""
+                } else {
+                  result(0).jbState
+                }
+              }
+          }
+        }
+        workbook.save(appendToFileName(inputFile(), ".upd"))
+      }
+    }
+
+  addSubCommand(doc)
+  addSubCommand(up)
 }
 
 object Main {
