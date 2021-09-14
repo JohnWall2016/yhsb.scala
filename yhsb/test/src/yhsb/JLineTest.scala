d306279c331b9075d67744b86dccc2ea6fac810c
JohnWall2016 <5672618@qq.com>
2021-09-14 17:10:09 +0800

    update

--- yhsb/test/src/yhsb/JLineTest.scala
+++ yhsb/test/src/yhsb/JLineTest.scala
@@ -1,45 +1,16 @@
 package yhsb
 
 import utest.{TestSuite, Tests, test}
-import org.jline.reader.impl.DefaultParser
-import org.jline.terminal.TerminalBuilder
-import org.jline.reader.LineReaderBuilder
-import yhsb.base.text.Parser
-import org.jline.reader.impl.history.DefaultHistory
-import org.jline.reader.LineReader
-import java.nio.file.Paths
-import java.lang.System
+import yhsb.base.io.Repl
 
 object JLineTest extends TestSuite {
   def tests =
     Tests {
       test("run") {
-        var args: Seq[String] = null
-
-        val parser = new DefaultParser()
-        parser.setEscapeChars(null)
-
-        val terminal = TerminalBuilder
-          .builder()
-          .system(true)
-          .build()
-
-        val reader = LineReaderBuilder
-          .builder()
-          .terminal(terminal)
-          .parser(parser)
-          .history(new DefaultHistory)
-          .variable(
-            LineReader.HISTORY_FILE,
-            Paths.get(System.getProperty("user.home"), ".yhsb")
-          )
-          .build()
-
-        do {
-          val line = reader.readLine("> ")
-          args = Parser.parseLine(line)
-          terminal.writer().println(args)
-        } while (!(args.length == 1 && (args(0) == ":q" || args(0) == ":quit")))
+        Repl.runLoop { args =>
+          println(args)
+          true
+        }
       }
     }
 }
