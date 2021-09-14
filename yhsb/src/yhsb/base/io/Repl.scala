1e8a134c9f18d3f7d9b17607616a7753c1951c2a
JohnWall2016 <5672618@qq.com>
2021-09-14 17:25:06 +0800

    update

--- yhsb/src/yhsb/base/io/Repl.scala
+++ yhsb/src/yhsb/base/io/Repl.scala
@@ -3,42 +3,62 @@ package yhsb.base.io
 import java.lang.System
 import java.nio.file.Paths
 
+import org.jline.reader.EndOfFileException
 import org.jline.reader.LineReader
 import org.jline.reader.LineReaderBuilder
+import org.jline.reader.UserInterruptException
 import org.jline.reader.impl.DefaultParser
 import org.jline.reader.impl.history.DefaultHistory
 import org.jline.terminal.TerminalBuilder
+
 import yhsb.base.text.Parser
 
 object Repl {
-  def runLoop(action: Seq[String] => Boolean) = {
-    var args: Seq[String] = null
+  def runLoop(
+      action: Seq[String] => Boolean,
+      initialize: => Unit = {},
+      finalize: => Unit = {}
+  ) = {
+    initialize
+    try {
+      var args: Seq[String] = null
 
-    val parser = new DefaultParser()
-    parser.setEscapeChars(null)
+      val parser = new DefaultParser()
+      parser.setEscapeChars(null)
 
-    val terminal = TerminalBuilder
-      .builder()
-      .system(true)
-      .build()
+      val terminal = TerminalBuilder
+        .builder()
+        .system(true)
+        .build()
 
-    val reader = LineReaderBuilder
-      .builder()
-      .terminal(terminal)
-      .parser(parser)
-      .history(new DefaultHistory)
-      .variable(
-        LineReader.HISTORY_FILE,
-        Paths.get(System.getProperty("user.home"), ".yhsb")
-      )
-      .build()
+      val reader = LineReaderBuilder
+        .builder()
+        .terminal(terminal)
+        .parser(parser)
+        .history(new DefaultHistory)
+        .variable(
+          LineReader.HISTORY_FILE,
+          Paths.get(System.getProperty("user.home"), ".yhsb_history")
+        )
+        .build()
 
-    var continue = true
-    do {
-      val line = reader.readLine("> ")
-      args = Parser.parseLine(line)
-      continue = action(args) &&
-        !(args.length == 1 && (args(0) == ":q" || args(0) == ":quit"))
-    } while (continue)
+      try {
+        var continue = true
+        while (continue) {
+          try {
+            val line = reader.readLine("> ")
+            args = Parser.parseLine(line)
+            continue = action(args) &&
+              !(args.length == 1 && (args(0) == ":q" || args(0) == ":quit"))
+          } catch {
+            case _: UserInterruptException =>
+          }
+        }
+      } catch {
+        case _: EndOfFileException => println("Byte!")
+      }
+    } finally {
+      finalize
+    }
   }
 }
